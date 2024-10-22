c7b8e4b1f1c54bc1b46a7b54adb4a7dca24e2d6a
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2024-10-22 16:15:03 +0200

    Added example & sample

---
+++ Example/Example/Example/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Example
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.BOpen = new System.Windows.Forms.Button();
+            this.BClear = new System.Windows.Forms.Button();
+            this.BSave = new System.Windows.Forms.Button();
+            this.loading = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.loading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(12, 12);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(610, 530);
+            this.TextBox.TabIndex = 0;
+            this.TextBox.Text = "";
+            // 
+            // BOpen
+            // 
+            this.BOpen.Location = new System.Drawing.Point(646, 13);
+            this.BOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.BOpen.Name = "BOpen";
+            this.BOpen.Size = new System.Drawing.Size(112, 82);
+            this.BOpen.TabIndex = 2;
+            this.BOpen.Text = "Open RTF File";
+            this.BOpen.UseVisualStyleBackColor = true;
+            this.BOpen.Click += new System.EventHandler(this.BOpen_Click);
+            // 
+            // BClear
+            // 
+            this.BClear.Location = new System.Drawing.Point(646, 230);
+            this.BClear.Margin = new System.Windows.Forms.Padding(4);
+            this.BClear.Name = "BClear";
+            this.BClear.Size = new System.Drawing.Size(112, 82);
+            this.BClear.TabIndex = 3;
+            this.BClear.Text = "Clear";
+            this.BClear.UseVisualStyleBackColor = true;
+            this.BClear.Click += new System.EventHandler(this.BClear_Click);
+            // 
+            // BSave
+            // 
+            this.BSave.Location = new System.Drawing.Point(646, 122);
+            this.BSave.Margin = new System.Windows.Forms.Padding(4);
+            this.BSave.Name = "BSave";
+            this.BSave.Size = new System.Drawing.Size(112, 82);
+            this.BSave.TabIndex = 4;
+            this.BSave.Text = "Save as PDF";
+            this.BSave.UseVisualStyleBackColor = true;
+            this.BSave.Click += new System.EventHandler(this.BSave_Click);
+            // 
+            // loading
+            // 
+            this.loading.Image = ((System.Drawing.Image)(resources.GetObject("loading.Image")));
+            this.loading.Location = new System.Drawing.Point(97, 122);
+            this.loading.Name = "loading";
+            this.loading.Size = new System.Drawing.Size(438, 285);
+            this.loading.TabIndex = 10;
+            this.loading.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(783, 554);
+            this.Controls.Add(this.loading);
+            this.Controls.Add(this.BSave);
+            this.Controls.Add(this.BClear);
+            this.Controls.Add(this.BOpen);
+            this.Controls.Add(this.TextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form";
+            this.Text = "Form";
+            ((System.ComponentModel.ISupportInitialize)(this.loading)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button BOpen;
+        private System.Windows.Forms.Button BClear;
+        private System.Windows.Forms.Button BSave;
+        private System.Windows.Forms.PictureBox loading;
+    }
+}
+
---
+++ Example/Example/Example/Form1.cs
@@ -0,0 +1,135 @@
+﻿using iText.Html2pdf;
+using RtfPipe;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static iText.Kernel.Pdf.Colorspace.PdfDeviceCs;
+
+namespace Example
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        //----FIELDS----
+        private string RTFpath = null;
+        private string savePath = null;
+
+        //----BUILDER----
+        public Form()
+        {
+            InitializeComponent();
+
+            //Hide loading gif
+            this.loading.Hide();
+        }
+
+        //----PRIVATE METHODS----
+
+        //Change element on a string
+        private string ChangeString(string original, string find, string replace)
+        {
+            StringBuilder builder = new StringBuilder(original);
+            builder.Replace(find, replace);
+            return builder.ToString();
+        }
+
+        //----OPEN BUTTON----
+        private void BOpen_Click(object sender, EventArgs e)
+        {
+            //Show loading gif
+            this.loading.Show();
+
+            // Create an OpenFileDialog to request a file to open.
+            OpenFileDialog openFile1 = new OpenFileDialog();
+
+            // Initialize OpenFileDialog to open pdf files.
+            openFile1.DefaultExt = "*.rtf";
+            openFile1.Filter = "RTF Files|*.rtf";
+
+            // Determine whether the user selected a file from the OpenFileDialog. 
+            if (openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               openFile1.FileName.Length > 0)
+            {
+                this.RTFpath = openFile1.FileName;
+            }
+
+            this.TextBox.LoadFile(this.RTFpath);
+
+            //Hide loading gif
+            this.loading.Hide();
+        }
+
+        private void BSave_Click(object sender, EventArgs e)
+        {
+            //Show loading gif
+            this.loading.Show();
+
+            //Check if RTB is empty
+            if (this.TextBox.Rtf.Length != 0)
+            {
+
+                if (this.RTFpath != null)
+                {
+                    //Open dialog in wau to select where save
+                    System.Windows.Forms.SaveFileDialog saveFileDialog1;
+                    saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+                    //Set default extension
+                    saveFileDialog1.DefaultExt = "pdf";
+                    //Set dafult path where save
+                    saveFileDialog1.FileName = ChangeString(this.RTFpath, ".rtf", " ");
+                    DialogResult dr = saveFileDialog1.ShowDialog();
+                    if (dr == DialogResult.OK)
+                    {
+                        this.savePath = saveFileDialog1.FileName;
+                    }
+                }
+                else 
+                {
+                    //Open dialog in wau to select where save
+                    System.Windows.Forms.SaveFileDialog saveFileDialog1;
+                    saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+                    DialogResult dr = saveFileDialog1.ShowDialog();
+                    if (dr == DialogResult.OK)
+                    {
+                        this.savePath = saveFileDialog1.FileName;
+                    }
+                }
+
+                try
+                {
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                    HtmlConverter.ConvertToPdf(Rtf.ToHtml(this.TextBox.Rtf.ToString()), new FileStream(this.savePath, FileMode.Create));
+                }
+                catch 
+                {
+                    MessageBox.Show("Problems during the saving!");
+                }
+
+                if (File.Exists(this.savePath))
+                {
+                    MessageBox.Show("The file has been saved successfully");
+                }
+                
+            }
+            else 
+            {
+                MessageBox.Show("No document to save");
+            }
+
+            //Hide loading gif
+            this.loading.Hide();
+        }
+
+        //----CLEAR BUTTON----
+        private void BClear_Click(object sender, EventArgs e)
+        {
+            this.TextBox.Clear();
+        }
+    }
+}
